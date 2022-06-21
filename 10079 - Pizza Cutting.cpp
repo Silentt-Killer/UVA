a0e95b6d8eedf003d69d35bb04b86d6eaa814099
Md Redowanul Islam <104225343+Silentt-Killer@users.noreply.github.com>
2022-06-22 03:29:49 +0600

    Create 10079 - Pizza Cutting.cpp

---
+++ 10079 - Pizza Cutting.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    long long n;
+    while (scanf("%lld",&n),n>=0){
+        printf("%lld\n",(n*(n+1)/2+1));
+    }
+    return 0;
+}
